f900efb0b1137579387360e2219fc315ce98696e
nouamane <nimobitor@gmail.com>
2023-09-16 23:17:05 +0100

    make note adding use a modal instead of the default browser prompt

--- js/content.js
+++ js/content.js
@@ -1,8 +1,56 @@
 let highlightedElement = null;
 const originalHrefs = [];
+let isAddingTicket = false;
+let ticketPosition = {x: 0, y: 0};
+const injectStarterHTML = function () {
+    const modalHTML = `
+      <div id="myModal" class="modal">
+        <div class="modal-content">
+          <p>Note</p>
+          <input type="text" id="noteInput" placeholder="Enter your note...">
+          <button id="submitButton">Submit</button>
+        </div>
+      </div>
+    `;
+    const container = document.createElement('div');
+    container.innerHTML = modalHTML;
+    document.body.appendChild(container);
+}
+injectStarterHTML()
+
+
+const modal = document.getElementById('myModal');
+const submitButton = document.getElementById('submitButton');
+const submitNote = function () {
+    const noteInput = document.getElementById('noteInput');
+    const note = noteInput.value;
+    // Process the note as needed (e.g., save it, display it, etc.)
+    console.log('Note submitted:', note);
+    postStickyNote(note)
+    closeModal();
+}
+
+submitButton.addEventListener('click', submitNote);
+
+// Open the modal
+function openModal(event) {
+    if(!isAddingTicket) {
+        modal.style.display = 'block';
+        ticketPosition = {x: event.pageX, y: event.pageY}
+        isAddingTicket = true
+    }
+}
+
+// Close the modal
+function closeModal() {
+    const noteInput = document.getElementById('noteInput');
+    noteInput.value = '';
+    modal.style.display = 'none';
+    isAddingTicket = false
+}
 
 const highlight = function (event) {
-    if(event.target.className !== 'sticky-note-element' && event.target.className !== 'highlighted-element') {
+    if (event.target.className !== 'sticky-note-element' && event.target.className !== 'highlighted-element' && !isAddingTicket) {
         if (highlightedElement) {
             highlightedElement.style.outline = '';
         }
@@ -11,11 +59,11 @@ const highlight = function (event) {
     }
 }
 
-const postStickyNote = function (event) {
+
+const postStickyNote = function (note) {
     console.log(`clicked an element ${highlightedElement}`)
     if (highlightedElement) {
         console.log("will add sticky note")
-        const note = prompt('Enter your note:');
         if (note) {
             const sticky = document.createElement('div');
             const stickyId = `sticky-note-element-${Date.now()}`
@@ -25,35 +73,33 @@ const postStickyNote = function (event) {
             sticky.style.background = 'yellow';
             sticky.style.zIndex = '9999';
             document.body.appendChild(sticky);
-            sticky.style.left = event.pageX + 'px';
-            sticky.style.top = event.pageY + 'px';
+            sticky.style.left = ticketPosition.x + 'px';
+            sticky.style.top = ticketPosition.y + 'px';
             sticky.style.display = 'block'
             sticky.id = stickyId
-            event.target.style.backgroundColor = '#e0b926'
-            event.target.style.cursor = 'pointer'
-            event.target.className = 'highlighted-element'
-            event.target.addEventListener('click', function() {
+            highlightedElement.style.backgroundColor = '#e0b926'
+            highlightedElement.style.cursor = 'pointer'
+            highlightedElement.className = 'highlighted-element'
+            highlightedElement.addEventListener('click', function () {
                 toggleStickyVisibility(stickyId);
             });
             clearStickyNoteAdding();
         }
     }
-    event.preventDefault();
-    event.stopPropagation();
 }
 
 const toggleStickyVisibility = function (id) {
     console.log(`trying to find element with id ${id}`)
     const element = document.getElementById(id)
-    if(element.style.display === 'none')
+    if (element.style.display === 'none')
         element.style.display = 'block'
     else
         element.style.display = 'none'
 }
 
-const clearStickyNoteAdding = function (){
-    document.removeEventListener('mouseover',highlight)
-    document.removeEventListener('click',postStickyNote)
+const clearStickyNoteAdding = function () {
+    document.removeEventListener('mouseover', highlight)
+    document.removeEventListener('click', openModal)
     if (highlightedElement) {
         highlightedElement.style.outline = '';
     }
@@ -62,18 +108,24 @@ const clearStickyNoteAdding = function (){
         link.href = originalHrefs[index];
     });
 }
-const initiateStickyNoteAdding = function(){
+const initiateStickyNoteAdding = function () {
     const links = document.querySelectorAll('a');
     links.forEach(link => {
         originalHrefs.push(link.href); // Store original href
         link.removeAttribute('href'); // Disable the link
     });
-    document.addEventListener('click', postStickyNote);
+    document.addEventListener('click', openModal);
     document.addEventListener('mouseover', highlight);
 }
 
-chrome.runtime.onMessage.addListener(function(message) {
+chrome.runtime.onMessage.addListener(function (message) {
     if (message === "injectScript") {
         initiateStickyNoteAdding()
     }
 })
+
+window.addEventListener('click', function (event) {
+    if (event.target === modal) {
+        closeModal();
+    }
+});
